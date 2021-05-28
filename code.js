b0189ee7d108936fd794502f15a6688e3f0ef789
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-28 11:42:42 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,15 @@
-function simulation_frame() {
-
+function simulation_frame(particles_array) {
+    return particles_array;
 }
 
 function render_output(particles_array) {
     var svg = document.querySelector("svg");
     var svgns = "http://www.w3.org/2000/svg";
-    for (particle of particles_array) {
+    for (var particle of particles_array) {
         var img = document.createElementNS(svgns, "image");
+        img.setAttribute("x", particle.x - particle.w);
+        img.setAttribute("y", particle.y - particle.h);
+        img.setAttribute("width", particle.w);
+        img.setAttribute("height", particle.h);
     }
 }
