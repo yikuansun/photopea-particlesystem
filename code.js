c63f5687231ff1d0bd3d8e4ec5aad0fb6d2377fa
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-30 21:39:03 -0400

    Fix rotation

--- code.js
+++ code.js
@@ -30,7 +30,7 @@ function render_output(particles_array) {
         img.setAttribute("y", particle.y - particle.h);
         img.setAttribute("width", particle.w);
         img.setAttribute("height", particle.h);
-        img.setAttribute("transform", `rotate(${particle.angle * 180 / Math.PI})`);
+        img.setAttribute("transform", `rotate(${particle.angle * 180 / Math.PI} ${particle.x} ${particle.y})`);
         svg.appendChild(img);
     }
     document.body.appendChild(svg);
